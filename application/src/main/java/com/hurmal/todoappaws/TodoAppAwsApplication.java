1abef9046d2dd18d2883caeb0c66b3630277b584
Davian Hurmal <davian.hurmal@gmail.com>
2022-02-23 19:25:26 -0700

    Initial commit

---
+++ application/src/main/java/com/hurmal/todoappaws/TodoAppAwsApplication.java
@@ -0,0 +1,13 @@
+package com.hurmal.todoappaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoAppAwsApplication.class, args);
+    }
+
+}
---
+++ application/src/test/java/com/hurmal/todoappaws/TodoAppAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hurmal.todoappaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
